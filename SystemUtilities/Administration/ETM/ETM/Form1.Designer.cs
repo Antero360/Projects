b8d86b05ee9f0b88e16faf075a67817ff9a516b0
Poet360 <nevarez.antero@gmail.com>
2023-04-19 17:14:37 -0500

    Removed spaces from directories

---
+++ SystemUtilities/Administration/ETM/ETM/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ETM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.task2Run = new System.Windows.Forms.TextBox();
+            this.startProcess = new System.Windows.Forms.Button();
+            this.killProcess = new System.Windows.Forms.Button();
+            this.totalProcesses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // processView
+            // 
+            this.processView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.processView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.processView.Location = new System.Drawing.Point(258, 6);
+            this.processView.Name = "processView";
+            this.processView.Size = new System.Drawing.Size(393, 413);
+            this.processView.TabIndex = 0;
+            this.processView.UseCompatibleStateImageBehavior = false;
+            this.processView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "1";
+            this.columnHeader1.Text = "PID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Tag = "1";
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Tag = "1";
+            this.columnHeader3.Text = "Description";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Tag = "1";
+            this.columnHeader4.Text = "Status";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Tag = "1";
+            this.columnHeader5.Text = "Username";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Tag = "1";
+            this.columnHeader6.Text = "Memory (private working set)";
+            // 
+            // task2Run
+            // 
+            this.task2Run.Location = new System.Drawing.Point(12, 34);
+            this.task2Run.Name = "task2Run";
+            this.task2Run.Size = new System.Drawing.Size(207, 20);
+            this.task2Run.TabIndex = 1;
+            // 
+            // startProcess
+            // 
+            this.startProcess.Location = new System.Drawing.Point(12, 60);
+            this.startProcess.Name = "startProcess";
+            this.startProcess.Size = new System.Drawing.Size(75, 23);
+            this.startProcess.TabIndex = 2;
+            this.startProcess.Text = "Start";
+            this.startProcess.UseVisualStyleBackColor = true;
+            this.startProcess.Click += new System.EventHandler(this.StartProcess_Click);
+            // 
+            // killProcess
+            // 
+            this.killProcess.Location = new System.Drawing.Point(12, 94);
+            this.killProcess.Name = "killProcess";
+            this.killProcess.Size = new System.Drawing.Size(75, 23);
+            this.killProcess.TabIndex = 3;
+            this.killProcess.Text = "Kill";
+            this.killProcess.UseVisualStyleBackColor = true;
+            this.killProcess.Click += new System.EventHandler(this.KillProcess_Click);
+            // 
+            // totalProcesses
+            // 
+            this.totalProcesses.AutoSize = true;
+            this.totalProcesses.Location = new System.Drawing.Point(9, 6);
+            this.totalProcesses.Name = "totalProcesses";
+            this.totalProcesses.Size = new System.Drawing.Size(0, 13);
+            this.totalProcesses.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 425);
+            this.Controls.Add(this.totalProcesses);
+            this.Controls.Add(this.killProcess);
+            this.Controls.Add(this.startProcess);
+            this.Controls.Add(this.task2Run);
+            this.Controls.Add(this.processView);
+            this.Name = "Form1";
+            this.Text = "ETM (Emergency Task Manager)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.TextBox task2Run;
+        private System.Windows.Forms.Button startProcess;
+        private System.Windows.Forms.Button killProcess;
+        private System.Windows.Forms.Label totalProcesses;
+    }
+}
+
---
+++ SystemUtilities/Administration/ETM/ETM/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ETM
+{
+    public partial class Form1 : Form
+    {
+        private bool resizeFlag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.processView.SizeChanged += new EventHandler(ListView_SizeChange);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //resize the columns so that they have equal width
+            /*
+            foreach (ColumnHeader column in processView.Columns)
+            {
+                column.Width = (int)(processView.Width / 6);
+            }
+            */
+            ShowAllProcesses();
+        }
+
+        /*
+         * Summary: returns an Expando object representing a process
+         * with description and owner of process using the process identification number
+         */
+        public ExpandoObject GetMoreInfo(int pid)
+        {
+            //create dynamic object
+            dynamic process = new ExpandoObject();
+            process.Description = "";
+            process.Username = "Unknown";
+
+            //get a list of processes from Win32_Process
+            string query = String.Format("Select * From Win32_Process Where ProcessID = {0}",pid);
+            ManagementObjectSearcher queryEngine = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = queryEngine.Get();
+
+            //iterate through list and
+            foreach (ManagementObject processObject in processList)
+            {
+                //get username
+                string[] propertyList = new string[] {string.Empty, string.Empty};
+                int responseValue = Convert.ToInt32(processObject.InvokeMethod("GetOwner", propertyList));
+                if (responseValue == 0)
+                {
+                    process.Username = propertyList[0];
+                }
+
+                //get description if any
+                if (processObject["ExecutablePath"] != null)
+                {
+                    try
+                    {
+                        FileVersionInfo info = FileVersionInfo.GetVersionInfo(processObject["ExecutablePath"].ToString());
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            return process;
+        }
+
+        /*
+         * Summary: converts byte number to readable values
+         */
+        public string BytesToFormatValue(long originalValue)
+        {
+            //list of byte suffixes
+            List<string> suffixList = new List<string> {" B", " KB", " MB", " GB", " TB", " PB"};
+
+            for (int x = 0; x < suffixList.Count; x++)
+            {
+                long tempValue = (originalValue / ((int)Math.Pow(1024, (x + 1))));
+                if (tempValue == 0)
+                {
+                    return (originalValue / ((int)Math.Pow(1024, x))) + suffixList[x];
+                }
+            }
+            return originalValue.ToString();
+        }
+
+        /*
+         * Summary: renders all processes running on Windows to a listview 
+         */
+        public void ShowAllProcesses()
+        {
+            //use an array to hold all running processes
+            Process[] runningProcesses = Process.GetProcesses();
+
+            //show total processes count
+            //totalProcesses.Text = string.Format("{0} currently running.", runningProcesses.Length);
+            int processCount = 0;
+
+            //create a list to hold process icons
+            ImageList processIcons = new ImageList();
+
+            //iterate through all processes and show their information
+            foreach (Process process in runningProcesses)
+            {
+                //find the status of the process, is it responding or not
+                /*
+                 * the ?: operator, or conditional operator, replaces the if-else conditional statement
+                 * the format of this operator goes as follows:
+                 * boolean expression ? (response if expression is true) : (response if expression is false)
+                 */
+                string processStatus = (process.Responding == true ? "Responding" : "Not Responding" );
+
+                //get process extra information
+                dynamic processExtraInfo = GetMoreInfo(process.Id);
+
+                //create data source to bind to listview
+                string[] newProcessRow = {
+                    //col 1: process id
+                    process.Id.ToString(),
+                    //col 2: process name
+                    process.ProcessName,
+                    //col 3: process description
+                    processExtraInfo.Description,
+                    //col 4: process status
+                    processStatus,
+                    //col 5: process owner
+                    processExtraInfo.Username,
+                    //col 6: process memory usage
+                    BytesToFormatValue(process.PrivateMemorySize64),
+                    
+                };
+
+                //get process icon, if any
+                try
+                {
+                    processIcons.Images.Add(
+                        //unique identifier for the icon
+                        process.Id.ToString(),
+                        //get the icon and add to list
+                        Icon.ExtractAssociatedIcon(process.MainModule.FileName).ToBitmap()
+                    );
+                }
+                catch
+                {
+                }
+
+                //create a new row of information for the listview
+                ListViewItem displayRow = new ListViewItem(newProcessRow)
+                {
+                    //index the icon from icon list
+                    ImageIndex = processIcons.Images.IndexOfKey(process.Id.ToString())
+                };
+
+                //add the row to the listview
+                processCount++;
+                processView.Items.Add(displayRow);
+                totalProcesses.Text = string.Format("{0} currently running.", processCount);
+            }
+
+            //set the large and small image lists to icon list
+            processView.LargeImageList = processIcons;
+            processView.SmallImageList = processIcons;
+        }
+
+        private void ClearProcessList()
+        {
+            processView.Items.Clear();
+            totalProcesses.Text = string.Format("{0} currently running.", 0);
+        }
+
+        private void ListView_SizeChange(object sender, EventArgs e)
+        {
+            //handles overlapping calls to SizeChange
+            if (!resizeFlag)
+            {
+                //raise the flag
+                resizeFlag = true;
+
+                ListView proView = sender as ListView;
+                if (proView != null)
+                {
+                    //get total sum of the width based on the column tag
+                    float totalTagSum = 0;
+                    for (int x = 0; x < proView.Columns.Count; x++)
+                        totalTagSum += Convert.ToInt32(proView.Columns[x].Tag);
+
+                    //calculate width of each column
+                    for (int x = 0; x < proView.Columns.Count; x++)
+                    {
+                        float colWidthPercent = (Convert.ToInt32(proView.Columns[x].Tag) / totalTagSum);
+                        proView.Columns[x].Width = (int)(colWidthPercent * proView.ClientRectangle.Width);
+                    }
+                }
+            }
+
+            //reset the resizing flag
+            resizeFlag = false;
+        }
+
+        private void StartProcess_Click(object sender, EventArgs e)
+        {
+            //make sure that user input has correct format
+            if (task2Run.Text.ToUpper().Contains(".EXE") == true)
+            {
+                try
+                {
+                    //start a new process
+                    Process process = new Process();
+                    process.StartInfo.FileName = task2Run.Text;
+                    process.Start();
+
+                    //clear current list and refresh
+                    ClearProcessList();
+
+                    MessageBox.Show("Process has been been initiated. Reloading list...");
+
+                    //reload list
+                    ShowAllProcesses();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(string.Format("ERROR! '{0}' is not a valid process.", task2Run.Text));
+                }
+            }
+            else
+            {
+
+                MessageBox.Show(string.Format("ERROR! '{0}' is not an executable process. Please make sure to enter a process name with the extension '.exe'.", task2Run.Text));
+            }
+        }
+
+        private void KillProcess_Click(object sender, EventArgs e)
+        {
+            //get the selected process
+            ListViewItem current = processView.SelectedItems[0];
+            Process process = Process.GetProcessById(int.Parse(current.Text.ToString()));
+            
+            //kill the selected process
+            process.Kill();
+
+            //clear list
+            ClearProcessList();
+
+            MessageBox.Show("Process has been killed successfully. Reloading list...");
+
+            //reload list
+            ShowAllProcesses();
+        }
+    }
+}
