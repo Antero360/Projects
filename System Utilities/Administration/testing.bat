586159f98d431bd7d7f683bf8f38ad829004691f
Poet360 <nevarez.antero@gmail.com>
2023-04-19 04:23:11 -0500

    testing batch file

---
+++ System Utilities/Administration/testing.bat
@@ -0,0 +1,9 @@
+:: set the path to scanner and path to results file. path to project is the path this batch file is located at
+:: set "pathToProject=%~dp0"
+:: set "pathToScanner=c:\users\antero360\source\tools\security\horusec\horusec.exe"
+:: set "pathToResults=%pathToProject%\testing-results.json"
+
+:: call command prompt to execute call to scanner 
+:: cmd /c "%pathToScanner% start -p %pathToProject% --output-format json --json-output-file %pathToResults% --disable-docker"
+
+echo this is only a test
