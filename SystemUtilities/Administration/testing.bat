25827a1dbf70a50b618d5be80120e6bf92619fc3
Antero Nevarez-Lira <nevarez.antero@gmail.com>
2023-04-28 04:06:13 -0500

    Delete testing.bat

--- SystemUtilities/Administration/testing.bat
+++
@@ -1,8 +0,0 @@
-:: set the path to scanner and path to results file. path to project is the path this batch file is located at
-set "pathToProject=%~dp0"
-set "pathToScanner=c:\users\antero360\source\tools\security\horusec\horusec.exe"
-set "pathToResults=%pathToProject%\testing-results.json"
-
-:: call command prompt to execute call to scanner 
-cmd.exe /k "%pathToScanner% start -p %pathToProject% --output-format json --json-output-file %pathToResults%"
-
